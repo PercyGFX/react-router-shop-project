5a9f63d71e4a3a58966c363b6ef8426e5616631d
PercyGFX <isurangabtk@gmail.com>
2023-06-17 01:10:45 +0530

    bg issue

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
 
     return (
 
-        <div className="flex flex-row justify-between bg-white shadow-md bg-gray-800">
+        <div className="flex flex-row justify-between shadow-md bg-gray-800">
             <div className="mx-[36px] my-[18px] m text-md font-semibold font-poppins text-white"> MOBILE SHOP </div>
 
             <div className="mx-[36px] my-[18px] m text-sm font-light font-poppins text-white"> Copyright 2023 Mobile Shop </div>
