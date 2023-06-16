45855ce978b413e8e9e86dcf61f4ee3c249b49c0
PercyGFX <isurangabtk@gmail.com>
2023-06-16 15:36:30 +0530

    prepare talewind

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+      <h1 className="rotate-180 text-3xl bg-amber-600 flex flex-1 "
+           style={{ writingMode: 'vertical-rl' }}>
+        SUBSCRIBE AND CANCEL ANYTIME ANYWHERE
+      </h1>
+
+  );
+}
+
+export default App;
